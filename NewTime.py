02767356fd20791b5a4b73d7c8f03a878f06f59a
v_chliangxu <v_chliangxu@tencent.com>
2023-03-28 17:57:54 +0800

    定时发送提醒-机器人

---
+++ NewTime.py
@@ -0,0 +1,38 @@
+import time
+import requests
+import json
+
+class NewTime:
+
+    def get_now_time(self):
+        # 显示实时的时间戳
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+
+    def send_info(self):
+        # while True:
+        #     if self.get_now_time()
+        # https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=41757ab9-1285-419d-9386-08372b258a85
+        content = "高能已锁权限，可以开始烘焙了"
+        data = {
+            "msgtype": "text",
+            "text": {
+                "content": content,
+                "mentioned_list": ["v_chliangxu"],  # 被@的人
+            }
+        }
+        header = {
+            "Content-Type": "application/json"
+        }
+
+        # 发送企业微信消息
+        requests.post("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=41757ab9-1285-419d-9386-08372b258a85",
+                      data=json.dumps(data), headers=header)
+
+    def ko(self):
+        print(type(self.get_now_time()))
+        ret = json.loads(self.get_now_time())
+        print(ret)
+        return True
+if __name__ == '__main__':
+    print(NewTime().ko)
