81516c4542d21b46034e87e7bb0c739cae75fb2f
v_chliangxu <v_chliangxu@tencent.com>
2023-03-31 17:38:10 +0800

    任务的布置和数据库的预研

--- mysql/__init__.py
+++ mysql/__init__.py
@@ -1,2 +1,17 @@
 # mysql的windows安装
 # https://www.cnblogs.com/zhaoweixsj/p/16872784.html
+
+# 一些常用的mysql命令
+# 常见命令
+# show databases; 查看所有数据库
+# use 数据库名；使用该数据库库
+# select database(); 查看当前数据库的名称
+# create database 数据库名；创建数据库
+# 1.连接数据库
+# 1.1.连接自己的机器上的mysq
+# mysql -u root -p
+# 1.2.连接其他数据库上的mysql
+# mysql -h 所连接机器的ip -u root -p
+
+# 2.查询
+# 2.1.
