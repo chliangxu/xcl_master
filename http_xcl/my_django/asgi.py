ec3acffe853784032449da33c220bc3f1d7cafa2
v_chliangxu <v_chliangxu@tencent.com>
2023-10-20 15:14:49 +0800

    修改目录名

--- http_xcl/my_django/asgi.py
+++ http_xcl/my_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_django project.
+ASGI config for http_xcl project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'http_xcl.settings')
 
 application = get_asgi_application()
--- http_xcl/my_django/urls.py
+++ http_xcl/my_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_django project.
+URL configuration for http_xcl project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
