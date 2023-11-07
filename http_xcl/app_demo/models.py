50731b684ffa424d297c47f9b6bbd97387806e04
v_chliangxu <v_chliangxu@tencent.com>
2023-11-07 10:20:10 +0800

    删除无用文件

--- http_xcl/app_demo/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class title(models.Model):
-    title = models.CharField(max_length=20)#文章名
-    address = models.CharField(max_length=20)#地址
